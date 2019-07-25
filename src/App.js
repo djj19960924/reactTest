e5392fb453b3b53a49887d408efb9d9c8f6a8255
djj <djj@gitlab.com>
2019-07-25 17:30:39 +0800

    增加路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
-import { Button } from 'antd'
+import { BrowserRouter,Route,Switch } from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 /**
  * 应用根组件
  */
-
 export default class App extends Component {
 
     render() {
-        return <Button type="primary">Primary</Button>
+        return (
+            <BrowserRouter>
+                <Switch>{/**只匹配其中一个 */}
+                    <Route path='/login' component={Login}></Route>
+                    <Route path='/' component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
+        )
     }
 
 }
